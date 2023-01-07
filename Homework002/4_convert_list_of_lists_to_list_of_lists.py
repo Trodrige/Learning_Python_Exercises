7d479d782463aa495be2cf6f4f5a4d252d187109
Trodrige <tigrodrige@gmail.com>
2023-01-07 17:34:00 +0100

    Changes to Homework 2

--- Homework002/4_convert_list_of_lists_to_list_of_lists.py
+++ Homework002/4_convert_list_of_lists_to_list_of_lists.py
@@ -1,9 +1,10 @@
 #  4. Write a Python program to convert a given list of strings into list of lists using map function
 import ast
 
-inputString = input("Enter a list of strings to convert to list of lists: ")
+# inputString = input("Enter a list of strings to convert to list of lists: ")
+# inputList = list(ast.literal_eval(inputString))
 
-inputList = list(ast.literal_eval(inputString))
+inputList = ["I", "am", "a", "big", "boy"]
 
 output = list(map(list, inputList))
 
